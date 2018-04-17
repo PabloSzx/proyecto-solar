384672d33e835bdd815586f836659edd5db921fd
PabloSzx <pablo.saez@hotmail.com>
2018-04-17 15:49:41 -0300

    react-video implemented

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,17 +3,36 @@ import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 import styled from "styled-components";
 import logo from "../dist/logo.png";
 import GoogleDocsViewer from "react-google-docs-viewer";
-import Informe from "../dist/informe.pdf";
-const DIV = () => (
-	<div>
-		<Img src={logo} />
-		<GoogleDocsViewer
-			width="600px"
-			height="780px"
-			fileUrl="https://github.com/PabloSzx/proyecto-solar/raw/master/src/dist/informe.pdf"
-		/>
-	</div>
-);
+import YouTube from "react-youtube";
+
+class DIV extends Component {
+	_onReady(event) {
+		// access to player in all event handlers via event.target
+		event.target.playVideo();
+	}
+	render() {
+		const opts = {
+			height: "390",
+			width: "640",
+			playerVars: {
+				// https://developers.google.com/youtube/player_parameters
+				autoplay: 1
+			}
+		};
+
+		return (
+			<div>
+				<Img src={logo} />
+				<GoogleDocsViewer
+					width="600px"
+					height="780px"
+					fileUrl="https://github.com/PabloSzx/proyecto-solar/raw/master/src/dist/informe.pdf"
+				/>
+				<YouTube videoId="2g811Eo7K8U" opts={opts} onReady={this._onReady} />
+			</div>
+		);
+	}
+}
 
 const Img = styled.img`
 	position: absolute;
