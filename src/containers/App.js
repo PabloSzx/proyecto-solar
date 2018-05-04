af2fbde20281ea3e1a351d154fcaf85d679883ea
PabloSzx <pablo.saez@hotmail.com>
2018-05-04 01:01:13 -0300

    test

--- src/containers/App.js
+++ src/containers/App.js
@@ -6,106 +6,121 @@ import GoogleDocsViewer from "react-google-docs-viewer";
 import YouTube from "react-youtube";
 import Drawer from "./Drawer";
 const styles = {
-	center: {
-		position: "absolute",
-		top: "50%",
-		transform: "translateY(-50%)"
-	},
-	center2: {
-		position: "absolute",
-		top: "50%",
-		left: "50%",
-		transform: "translateX(-50%) translateY(-50%)"
-	}
+  center: {
+    position: "absolute",
+    top: "50%",
+    transform: "translateY(-50%)"
+  },
+  center2: {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translateX(-50%) translateY(-50%)"
+  }
 };
 
 let opts = {
-	height: "390",
-	width: "640",
-	playerVars: {
-		// https://developers.google.com/youtube/player_parameters
-		autoplay: 1
-	}
+  height: "390",
+  width: "640",
+  playerVars: {
+    // https://developers.google.com/youtube/player_parameters
+    autoplay: 1
+  }
 };
 
 class DIV extends Component {
-	constructor(props) {
-		super(props);
+  constructor(props) {
+    super(props);
 
-		this.state = {
-			page: "informe",
-			width: 0,
-			height: 0
-		};
-	}
-	_onReady(event) {
-		// access to player in all event handlers via event.target
-		// event.target.playVideo();
-	}
+    this.state = {
+      page: "informe",
+      width: 0,
+      height: 0
+    };
+  }
+  _onReady(event) {
+    // access to player in all event handlers via event.target
+    // event.target.playVideo();
+  }
 
-	setPage(page) {
-		console.log(page);
-		this.setState({
-			page
-		});
-	}
+  setPage(page) {
+    console.log(page);
+    this.setState({
+      page
+    });
+  }
 
-	renderPage() {
-		switch (this.state.page) {
-			case "home": {
-				return <div />;
-			}
-			case "informe": {
-				return (
-					<GoogleDocsViewer
-						width="100vh"
-						height="98vh"
-						fileUrl="https://github.com/PabloSzx/proyecto-solar/raw/master/src/dist/informe.pdf"
-					/>
-				);
-			}
-			case "video": {
-				return (
-					<YouTube videoId="sr4lSBuYV_w" opts={opts} onReady={this._onReady} />
-				);
-			}
-			default: {
-				return <div />;
-			}
-		}
-	}
+  renderPage() {
+    switch (this.state.page) {
+      case "home": {
+        return <div />;
+      }
+      case "informe1": {
+        return (
+          <GoogleDocsViewer
+            width="100vh"
+            height="98vh"
+            fileUrl="https://github.com/PabloSzx/proyecto-solar/raw/master/src/dist/informe.pdf"
+          />
+        );
+      }
+      case "informe2": {
+        return (
+          <GoogleDocsViewer
+            width="100vh"
+            height="98vh"
+            fileUrl="https://github.com/PabloSzx/proyecto-solar/raw/master/src/dist/informe.pdf"
+          />
+        );
+      }
 
-	render() {
-		return (
-			<div>
-				<Drawer setPage={page => this.setPage(page)} />
-				<Img src={logo} />
-				<div style={styles.center2} />
-				<div style={styles.center2}>{this.renderPage()}</div>
-			</div>
-		);
-	}
+      case "video1": {
+        return (
+          <YouTube videoId="sr4lSBuYV_w" opts={opts} onReady={this._onReady} />
+        );
+      }
+      case "video2": {
+        return (
+          <YouTube videoId="eX4I6PbLJls" opts={opts} onReady={this._onReady} />
+        );
+      }
+      default: {
+        return <div />;
+      }
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <Drawer setPage={page => this.setPage(page)} />
+        <Img src={logo} />
+        <div style={styles.center2} />
+        <div style={styles.center2}>{this.renderPage()}</div>
+      </div>
+    );
+  }
 }
 
 const Img = styled.img`
-	position: fixed;
-	right: 1%;
-	top: 1%;
-	width: 20vw;
-	z-index: -1;
+  position: fixed;
+  right: 1%;
+  top: 1%;
+  width: 20vw;
+  z-index: -1;
 `;
 
 class App extends Component {
-	render() {
-		return (
-			<BrowserRouter>
-				<Switch>
-					<Route exact path="/" component={DIV} />
-					<Redirect from="/*" to="/" />
-				</Switch>
-			</BrowserRouter>
-		);
-	}
+  render() {
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={DIV} />
+          <Redirect from="/*" to="/" />
+        </Switch>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
--- src/containers/Drawer.js
+++ src/containers/Drawer.js
@@ -8,35 +8,49 @@ import DescriptionIcon from "@material-ui/icons/Description";
 import VideoIcon from "@material-ui/icons/OndemandVideo";
 
 class PermanentDrawer extends Component {
-	render() {
-		return (
-			<Drawer variant="permanent" anchor="left">
-				<Divider />
-				<List
-					style={{
-						width: "15vw",
-						position: "relative",
-						top: "50%",
-						transform: "perspective(1px) translateY(-50%)"
-					}}
-				>
-					<ListItem button onClick={() => this.props.setPage("informe")}>
-						<ListItemIcon>
-							<DescriptionIcon />
-						</ListItemIcon>
-						<ListItemText primary="Informe" />
-					</ListItem>
-					<Divider />
-					<ListItem button onClick={() => this.props.setPage("video")}>
-						<ListItemIcon>
-							<VideoIcon />
-						</ListItemIcon>
-						<ListItemText primary="Video" />
-					</ListItem>
-				</List>
-			</Drawer>
-		);
-	}
+  render() {
+    return (
+      <Drawer variant="permanent" anchor="left">
+        <Divider />
+        <List
+          style={{
+            width: "15vw",
+            position: "relative",
+            top: "50%",
+            transform: "perspective(1px) translateY(-50%)"
+          }}
+        >
+          <ListItem button onClick={() => this.props.setPage("informe1")}>
+            <ListItemIcon>
+              <DescriptionIcon />
+            </ListItemIcon>
+            <ListItemText primary="Informe Tarea 1" />
+          </ListItem>
+          <Divider />
+          <ListItem button onClick={() => this.props.setPage("video1")}>
+            <ListItemIcon>
+              <VideoIcon />
+            </ListItemIcon>
+            <ListItemText primary="Video Tarea 1" />
+          </ListItem>
+          <Divider />
+          <ListItem button onClick={() => this.props.setPage("informe2")}>
+            <ListItemIcon>
+              <VideoIcon />
+            </ListItemIcon>
+            <ListItemText primary="Informe Tarea 2" />
+          </ListItem>
+          <Divider />
+          <ListItem button onClick={() => this.props.setPage("video2")}>
+            <ListItemIcon>
+              <VideoIcon />
+            </ListItemIcon>
+            <ListItemText primary="Video Tarea 2" />
+          </ListItem>
+        </List>
+      </Drawer>
+    );
+  }
 }
 
 export default withStyles(null)(PermanentDrawer);
